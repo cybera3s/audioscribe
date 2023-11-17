909a8d1368c2472c871a7bea0ce653623fd5fbae
cybera3s <cybera.3s@gmail.com>
2023-11-17 11:53:46 +0330

    feat: add format converter

---
+++ app/utils/voice_to_text.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+
+from format_converter import convert_to_wav_format
+
+
+
+def voice_to_text(input_path, output_path, lang: str) -> str:
+    
+    convert_to_wav_format(str(input_path), str(output_path))
+
+    # Recognizing Voice
+    recognizer = sr.Recognizer()
+    audioFile = sr.AudioFile(str(output_path))
+
+    with audioFile as source:
+        data = recognizer.record(source)
+
+    # recognizing voice using Google API
+    try:
+        transcript = recognizer.recognize_google(
+            data,
+            key=None,
+            language=lang,
+        )
+    except Exception as e:
+        return str(e)
+
+    # removing audio files
+    input_path.unlink()
+    output_path.unlink()
+
+    return transcript
