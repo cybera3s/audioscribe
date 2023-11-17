7af0bf502c152f9d8030887e6477081e435d7eaf
cybera3s <cybera.3s@gmail.com>
2023-11-17 16:25:56 +0330

    refactor

--- app/utils/voice_to_text.py
+++ app/utils/voice_to_text.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from speech_recognition import RequestError
 
 from utils.format_converter import convert_to_wav_format
 from monitoring.main_logging import get_logger
@@ -6,15 +7,25 @@ from monitoring.main_logging import get_logger
 logger = get_logger(__name__)
 
 
-def turn_voice_to_text(input_path, output_path, speach_lang: str) -> str:
+def turn_voice_to_text(
+    input_path, output_path, speach_lang: str, duration: int = 60
+) -> str:
+    """
+    Convert voice to text
+    """
+
+    # convert voice to wav format
     convert_to_wav_format(str(input_path), str(output_path))
+
+    error_message: str = ""
 
     # Recognizing Voice
     recognizer = sr.Recognizer()
     audioFile = sr.AudioFile(str(output_path))
 
     with audioFile as source:
-        data = recognizer.record(source)
+        recognizer.adjust_for_ambient_noise(source)
+        data = recognizer.record(source, duration=duration)
 
     # recognizing voice using Google API
     try:
@@ -23,8 +34,19 @@ def turn_voice_to_text(input_path, output_path, speach_lang: str) -> str:
             key=None,
             language=speach_lang,
         )
+
+    # internet problem or key error
+    except RequestError as e:
+        logger.error(f"Error with internet or key\n" f"{str(e)}")
+
+        error_message = "Error happened when converting"
+        return error_message
+
     except Exception as e:
-        return str(e)
+        logger.error(f"Error happened\n" f"{str(e)}")
+
+        error_message = "Error happened when converting"
+        return error_message
 
     # removing audio files
     input_path.unlink()
