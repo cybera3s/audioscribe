ba271f25ad025ec9db173f796d1a8469ecbbc34e
cybera3s <cybera.3s@gmail.com>
2023-11-17 16:25:48 +0330

    refactor

--- app/utils/format_converter.py
+++ app/utils/format_converter.py
@@ -1,18 +1,24 @@
 import ffmpeg
+from monitoring.main_logging import get_logger
+
+
+logger = get_logger(__name__)
 
 
 def convert_to_wav_format(input, output) -> bool:
     """
-    Converts input file to provided output format
+    Converts input file to wav format
 
     Return:
         True if has no err else False
     """
 
     stream = ffmpeg.input(input)
-    stream = ffmpeg.output(stream, output)
-    out, err = ffmpeg.run(stream)
+    stream = ffmpeg.output(stream, output, format="wav")
+    out, err = ffmpeg.run(stream, capture_stdout=True, overwrite_output=True)
 
     if not err:
         return True
-    return False
+    else:
+        logger.error(err)
+        return False
